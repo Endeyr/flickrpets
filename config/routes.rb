3cf7b05535b439ff0242696e4dea57b00143cab5
Endeyr <aaron.thompson42594@gmail.com>
2022-07-24 11:42:39 -0400

    added static pages scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  resources :static_pages
   mount Sidekiq::Web => "/sidekiq" if defined?(Sidekiq) # monitoring console
   root "home#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
---
+++ test/models/static_page_test.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: static_pages
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require "test_helper"
+
+class StaticPageTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
